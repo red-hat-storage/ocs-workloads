ed13474320bf6dd131737472a1c23379cb9b0e50
Shylesh Kumar Mohan <shmohan@redhat.com>
2022-06-23 12:27:07 -0700

    Update script location in deployment yaml
    Signed-off-by: Shylesh Kumar Mohan <shmohan@redhat.com>

--- rdr/busybox/scripts/simple_io.sh
+++ rdr/busybox/scripts/simple_io.sh
@@ -1,13 +1,13 @@
 while true
-	file=/mnt/test/data_`date +%s`
-	hashfile=/mnt/test/hashfile
-	do 
-		dd if=/dev/urandom of=$file bs=$(($RANDOM)) count=$(($RANDOM%3+1))
-		md5sum $file >>$hashfile
-		sync
-		sleep 15
-		if [ $(($RANDOM%2)) == 1 ]
-			then md5sum -c $hashfile
-		fi
-	done
-
+    file=/mnt/test/data_`date +%s`
+    hashfile=/mnt/test/hashfile
+    do
+        dd if=/dev/urandom of=$file bs=$(($RANDOM)) count=$(($RANDOM%3+1))
+        md5sum $file >>$hashfile
+        sync
+        sleep 15
+        if [ $(($RANDOM%2)) == 1 ]
+        then
+            md5sum -c $hashfile
+        fi
+    done
