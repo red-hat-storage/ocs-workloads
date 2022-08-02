b8ba3f42ebbdf769b72c734bb24ab1a176b432ab
shylesh <shmohan@redhat.com>
2022-08-02 13:23:24 -0700

    Busybox workload IOs and data integrity checks (#16)
    * 1.Introduce kernel untar workload along with data integrity check
    2. simple io integrity check
    3. Few changes in resource yamls which would help ocs-ci to invoke io scripts and integrity scripts on pods
    * Update kernel untar script to handle disk space
    * Test PR
    * test kernel untar
    * Remove bash specific statement
    * 1. Reduce disk full and free threshold to 45 and 20 respectively
    2. Add workload pattern labels in deployment yaml
    * Change channel and namespace for testing purpose
    * Change busybox image
    * Revert back all the test branch names and default values for config
    * Address review comments related to revert of default config
    * Revert channel PLACEHOLDER
    
    Signed-off-by: Shylesh Kumar Mohan <shmohan@redhat.com>
    
    Co-authored-by: prsurve <prsurve@redhat.com>

---
+++ rdr/busybox/scripts/simple_io_integrity_check.sh
@@ -0,0 +1,11 @@
+HASHFILE=/mnt/test/hashfile
+
+if [ -f "$HASHFILE" ]; then
+    echo "Found hashfile"
+else
+    echo "Hashfile not found, can't continue with integrity check"
+    exit 1
+fi
+
+md5sum -c --quiet $HASHFILE
+exit $?
