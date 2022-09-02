dbbe25062e56c09e62acfd36b2061055e2f63e1f
Pratik Surve <prsurve@redhat.com>
2022-09-02 14:50:32 +0530

    Use oflag=direct for dd io (#23)

--- rdr/busybox/scripts/simple_io.sh
+++ rdr/busybox/scripts/simple_io.sh
@@ -2,7 +2,7 @@ while true
     file=/mnt/test/data_`date +%s`
     hashfile=/mnt/test/hashfile
     do
-        dd if=/dev/urandom of=$file bs=$(($RANDOM)) count=$(($RANDOM%3+1))
+        dd if=/dev/urandom of=$file bs=$(($RANDOM)) count=$(($RANDOM%3+1)) oflag=direct
         md5sum $file >>$hashfile
         sync
         sleep $(($RANDOM%15+1))
